348b89832d508c316394fbf0372138e998ccc9cb
it21159480 <muhammedmufeel876@gmail.com>
2024-02-17 19:30:36 +0530

    enhanced the product list view

--- src/App.js
+++ src/App.js
@@ -4,28 +4,8 @@ import Header from './components/Header';
 import ProductListPage from './components/ProductListPage';
 
 function App() {
-  const img = 'https://hips.hearstapps.com/hmg-prod/images/hoka-zinal-13085-1643565794.jpg'
+
   return (
-    // <div className='App'>
-
-    //   <div>
-    //     <div className='circle'>
-
-    //     </div>
-    //     <Header />
-    //     <br /> <br /> <br /> <br />
-
-    //     <div className='circle-2'>
-
-    //     </div>
-    //     <ProductListPage/>
-    //     <Footer />
-    //   </div>
-
-
-
-
-    // </div>
     <div  className='App'>
       <div className='circle'></div>
       <div className='circle-1'></div>
@@ -34,6 +14,7 @@ function App() {
       <ProductListPage />
       <Footer />
     </div>
+   
   );
 }
 
--- src/components/ProductListPage.js
+++ src/components/ProductListPage.js
@@ -3,21 +3,30 @@ import productsData from '../data/products.json';
 import ProductCard from './ProductCard';
 import Pagination from './Pagination';
 import '../styles/ProductListPage.css'
-const itemsPerPage = 4; // Adjust the number of items per page
+const itemsPerPage = 8; // Adjust the number of items per page
 
 const ProductListPage = () => {
   const [currentPage, setCurrentPage] = useState(1);
 
+  // Calculate the index of the first and last items for the current page
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = productsData.slice(indexOfFirstItem, indexOfLastItem);
+  const rowDivideItem = indexOfFirstItem + 4 ;
 
+ 
+  const currentItems = productsData.slice(indexOfFirstItem, rowDivideItem);
+  const secondRow = productsData.slice(rowDivideItem, indexOfLastItem);
   const paginate = pageNumber => setCurrentPage(pageNumber);
 
   return (
     <div className='product-list-page'>
       <div className='product-list'>
         {currentItems.map(product => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+      <div className='product-list'>
+        {secondRow.map(product => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
